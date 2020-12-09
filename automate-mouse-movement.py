cb6697ef47e6b6ea368ac8263d06f393465304d4
Ricky Chan <chan.ricky.22@hotmail.com>
2020-12-09 11:53:35 -0500

    simple gui with start button to execute non threading version

--- automate-mouse-movement.py
+++ automate-mouse-movement.py
@@ -2,58 +2,58 @@ import keyboard
 import pyautogui
 from tkinter import *
 import time
-import threading
-
+import signal
+import sys
 
 import tkinter as tk
     
 switch = True
-
-def start():
-	def auto_move():
-		while switch == True:
-			print(pyautogui.position())
-			pyautogui.moveTo(766,376,duration = 1)
-			print(pyautogui.position())
-			time.sleep(10)
-
-			pyautogui.moveTo(927, 372, duration = 1)
-			print(pyautogui.position())
-			time.sleep(10)
-
-			pyautogui.moveTo(936, 531, duration = 1) 
-			print(pyautogui.position())
-			time.sleep(10)
-
-			pyautogui.moveTo(760, 518, duration = 1) 
-			print(pyautogui.position())
-			time.sleep(10)
-
-			if switch == False:
-				break
-	thread = threading.Thread(target = auto_move)
-	thread.start()
-def on():
-	switch == True
-	start()
-
+#set switch to False
+#causes frezze with Tkinter GUI due to while loop
+#Solution is to implement threading see threading branch
 def off():
 	switch ==  False
 
+#function to set switch to True
+#execute move function
+def on():
+	switch == True
+	auto_move()
+
+
+def auto_move():
+
+	#move in a square patern for the duration while loop is active
+	#trigger while loop with boolean switch
+	while switch:
+		print(switch)
+		print(pyautogui.position())
+		pyautogui.moveTo(766,376,duration = 1)
+		print(pyautogui.position())
+		time.sleep(10)
+
+		pyautogui.moveTo(927, 372, duration = 1)
+		print(pyautogui.position())
+		time.sleep(10)
+
+		pyautogui.moveTo(936, 531, duration = 1) 
+		print(pyautogui.position())
+		time.sleep(10)
+
+		pyautogui.moveTo(760, 518, duration = 1) 
+		print(pyautogui.position())
+		time.sleep(10)
+
+#creates basic GUI for script
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
+#stop button
 button = tk.Button(frame, 
-                   text="Stop", 
-                   fg="red",
-                   command=root.destroy)
-button.pack(side=tk.LEFT)
-slogan = tk.Button(frame,
-                   text="Start",
+                   text="Start", 
                    command=on)
-slogan.pack(side=tk.LEFT)
-
+button.pack(side=tk.LEFT)
 root.mainloop()
 
 """while True:
