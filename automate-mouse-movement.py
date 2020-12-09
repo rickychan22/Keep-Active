920e09854944d61032f3e1cbddd696d6ee29b9e0
Ricky Chan <chan.ricky.22@hotmail.com>
2020-12-08 21:03:39 -0500

    added code to detect keyboard input to execute and end script

--- automate-mouse-movement.py
+++ automate-mouse-movement.py
@@ -1,1 +1,14 @@
-print('hello world')
+import keyboard
+
+def auto_move():
+	print('Hello There')
+
+while True:
+	try: #if key is pressed start automatic mouse movemnet
+		if keyboard.is_pressed('q'):
+			# automatic movement code here
+			auto_move()
+		elif keyboard.is_pressed('w'):
+			break
+	except: # exits loop if key is pressed
+		break
